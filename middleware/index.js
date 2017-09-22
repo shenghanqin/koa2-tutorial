d459ff084bf5a2cc15065e26f1bccfbb262a11dd
brucecham <brucecham@qq.com>
2017-09-22 17:39:41 +0800

    add mi-error

--- middleware/index.js
+++ middleware/index.js
@@ -3,9 +3,14 @@ const ip = require("ip")
 
 const miLog = require('./mi-log')
 
+// const miHttpError = require('./mi-http-error')
+
 module.exports = (app) => {
 
-  
+  // app.use(miHttpError({
+  //   errorPageFolder: path.resolve(__dirname, '../errorPage') // 自定义错误文件夹
+  // })); 
+
   /**
    * 记录URL以及页面执行时间
    */
@@ -30,4 +35,14 @@ module.exports = (app) => {
     serverIp: ip.address()
   }));
 
+  // app.on("error", (err, ctx) => {
+  //   if (ctx && !ctx.headerSent && ctx.status < 500) {
+  //     ctx.status = 500
+  //   }
+  //   if (ctx && ctx.log && ctx.log.error) {
+  //     if (!ctx.state.logged) {
+  //       ctx.log.error(err.stack)
+  //     }
+  //   }
+  // })  
 }
